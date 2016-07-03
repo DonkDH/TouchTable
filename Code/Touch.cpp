56c2f62622345e21369d26c0f4f2be108a6d3c60
DonkDH <donk.dh@gmail.com>
2016-07-03 19:47:01 +0100

    More filter set up

--- Code/Touch.cpp
+++ Code/Touch.cpp
@@ -6,6 +6,9 @@ Touch::Touch() : m_active(false), m_location(0, 0), m_onEnd(false), m_onStart(fa
 	m_kalmanFilter = cv::KalmanFilter(4, 2);
 	m_kalmanUpdateMat = cv::Mat_<float>(2,1);
 
+	m_kalmanFilter.transitionMatrix = (cv::Mat_<float>(4, 4) << 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1);
+
+
 	cv::setIdentity(m_kalmanFilter.measurementMatrix);
 	cv::setIdentity(m_kalmanFilter.processNoiseCov, cv::Scalar::all(1e-4));
 	cv::setIdentity(m_kalmanFilter.measurementNoiseCov, cv::Scalar::all(10));
@@ -19,6 +22,17 @@ Touch::~Touch()
 
 void Touch::SetNewLocation(cv::Point loc)
 {
+	if (!m_active)
+	{
+		m_kalmanFilter.statePre.at<float>(0) = loc.x;
+		m_kalmanFilter.statePre.at<float>(1) = loc.y;
+
+		cv::setIdentity(m_kalmanFilter.measurementMatrix);
+		cv::setIdentity(m_kalmanFilter.processNoiseCov, cv::Scalar::all(1e-4));
+		cv::setIdentity(m_kalmanFilter.measurementNoiseCov, cv::Scalar::all(10));
+		cv::setIdentity(m_kalmanFilter.errorCovPost, cv::Scalar::all(.1));
+	}
+
 	m_active = true;
 
 	m_rawLocation = loc;
